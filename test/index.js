9b6194117922aac452c8a4f3619e197e33704a9e
Shane Bloomer <36099879+shanebloomer@users.noreply.github.com>
2018-09-24 13:59:53 +0100

    Remove regenerator-runtime dev dependency

--- test/index.js
+++ test/index.js
@@ -1,6 +1,3 @@
-// Required by Babel for async await syntax in tests.
-import 'regenerator-runtime/runtime'
-
 // Safari requires navigator.mediaDevices.getUserMedia to be called.
 import './set-up-safari'
 
