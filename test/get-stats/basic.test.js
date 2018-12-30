eebc15a817da614c3f595124314f0137e827ac67
Shane Bloomer <36099879+shanebloomer@users.noreply.github.com>
2018-12-30 10:23:22 +0000

    Add is instance of RTCStatsReport test

--- test/get-stats/basic.test.js
+++ test/get-stats/basic.test.js
@@ -3,7 +3,7 @@ import test from 'tape'
 import testUtil from '../test-util/'
 import WebRTCPeer from '../../src/'
 
-test('getStats() returns a promise', async assert => {
+test('getStats() returns a promise', assert => {
   // given
   const peerOne = new WebRTCPeer({ isInitiator: true })
   const peerTwo = new WebRTCPeer()
@@ -36,4 +36,17 @@ test('getStats() result size is greater than zero', async assert => {
     peerOne.destroy()
     peerTwo.destroy()
   })
+})
+
+test('getStats() returns an RTCStatsReport', async assert => {
+  // given
+  const peerOne = new WebRTCPeer({ isInitiator: true })
+  const peerTwo = new WebRTCPeer()
+
+  // then
+  assert.true(await peerOne.getStats() instanceof RTCStatsReport, 'is instance of RTCStatsReport')
+  assert.true(await peerTwo.getStats() instanceof RTCStatsReport, 'is instance of RTCStatsReport')
+  peerOne.destroy()
+  peerTwo.destroy()
+  assert.end()
 })
